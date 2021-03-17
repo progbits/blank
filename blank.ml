7e1067eb2b9cd390ba3a0b3046720faa69eadc5e
progbits <53880655+progbits@users.noreply.github.com>
2021-03-17 22:13:25 +0000

    Read all lines from an input file with some error handling

--- blank.ml
+++ blank.ml
@@ -1,4 +1,25 @@
-let () = 
-  let line = input_line stdin in
+open Core
+
+(* Lexical tokens *)
+type token =
+    | Space
+    | Tab
+    | LineFeed
+
+(* Get the input filename *)
+let get_filename =
+    try Some (Array.get (Sys.get_argv ()) 1)
+    with Invalid_argument _ -> None
+
+let read_lines file =
+    In_channel.read_lines file
+
+let print_line line =
     print_endline line
 
+let () =
+    match get_filename with
+    | Some filename ->
+        let lines = read_lines filename in
+            List.iter lines ~f:print_line
+    | None -> print_endline "usage: blank FILENAME"
