fb746162315dffc6cbd09d90749f002ee5b6fb45
progbits <53880655+progbits@users.noreply.github.com>
2021-03-20 18:26:47 +0000

    Use In_channel.read_all to simplify things a bit

--- blank.ml
+++ blank.ml
@@ -20,9 +20,6 @@ let get_filename =
     try Some (Array.get (Sys.get_argv ()) 1)
     with Invalid_argument _ -> None
 
-let read_lines file =
-    In_channel.read_lines file
-
 (* Convert a char to a token*)
 let char_to_token c = 
     match c with 
@@ -31,17 +28,13 @@ let char_to_token c =
     | '\n' -> LineFeed
     | _ -> raise (Invalid_argument "unexpected character")
 
-(* Scan tokens from a line *)
-let scan_line line = 
-    let chars = List.init (String.length line) ~f:(String.get line) in
-        List.map chars ~f:(fun c -> char_to_token c)
-
 (* Scan a list of tokens from a list of lines *)
-let scan lines = 
-    let tokens = List.concat (List.map lines ~f:scan_line) in
+let scan input =
+    let chars = List.init (String.length input) ~f:(String.get input) in
+    let tokens = List.map chars ~f:(fun c -> char_to_token c) in
         List.iter tokens ~f:(fun token -> print_token token)
 
 let () =
     match get_filename with
-    | Some filename -> scan (read_lines filename)
+    | Some filename -> scan (In_channel.read_all filename)
     | None -> print_endline "usage: blank FILENAME"
