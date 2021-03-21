85eb5276cf3b69428ab71f727397c46f1c66771f
progbits <53880655+progbits@users.noreply.github.com>
2021-03-21 21:22:04 +0000

    Add basic scanner test

---
+++ test/test.ml
@@ -0,0 +1,16 @@
+open OUnit2
+open Parse
+
+let input = " \t\n "
+
+let test_scan _ =
+  (* Check correct tokens are scanned. *)
+  assert_equal [Space; Tab; LineFeed; Space] (scan input)
+
+let suite =
+  "ScannerTests" >::: [
+    "test_scan" >:: test_scan
+  ]
+
+let () =
+  run_test_tt_main suite
